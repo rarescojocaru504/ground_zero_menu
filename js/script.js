5ac009589335a501c7bf8b4eb52f5fc04dccbfbc
rarescojocaru504 <rarescojocaru504@gmail.com>
2026-09-22 18:29:32 +0300

    restore design

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ function setLanguage(lang) {
     setTimeout(() => {
         document.querySelectorAll('[data-i18n]').forEach(el => {
             const key = el.getAttribute('data-i18n');
-            // Check if translations exist before setting innerHTML
             if (translations[lang] && translations[lang][key]) {
                 el.innerHTML = translations[lang][key]; 
             }
@@ -59,48 +58,7 @@ btnFood.addEventListener('click', () => {
     sectionDrinks.classList.add('hidden');
 });
 
-// Modal Logic pe element
-const modal = document.getElementById('item-modal');
-const modalTitle = document.getElementById('modal-title');
-const modalDesc = document.getElementById('modal-desc');
-const modalDetails = document.getElementById('modal-details');
-const modalImgContainer = document.getElementById('modal-img-container');
-const modalImage = document.getElementById('modal-image');
-const closeModal = document.querySelector('.close-modal');
-
-document.querySelectorAll('.trigger-modal').forEach(item => {
-    item.addEventListener('click', () => {
-        const key = item.getAttribute('data-key');
-        
-        // Extragem datele din dictionar pe baza cheii de baza (ex: 'easy_rider')
-        const titleText = translations[currentLang]['name_' + key] || '';
-        const descText = translations[currentLang]['desc_' + key] || '';
-        const detailsText = translations[currentLang]['details_' + key] || '';
-        const imgSrc = translations[currentLang]['img_' + key];
-
-        modalTitle.innerHTML = titleText;
-        modalDesc.innerHTML = descText;
-        modalDetails.innerHTML = detailsText;
-
-        // Daca exista o imagine in dictionar pt acest item, o afisam
-        if(imgSrc) {
-            modalImage.src = imgSrc;
-            modalImgContainer.classList.remove('hidden-img');
-        } else {
-            modalImgContainer.classList.add('hidden-img');
-        }
-
-        modal.classList.remove('hidden-modal');
-    });
-});
-
-closeModal.addEventListener('click', () => {
-    modal.classList.add('hidden-modal');
-});
-
-// Inchide modalul daca dai click in afara lui
-window.addEventListener('click', (e) => {
-    if (e.target === modal) {
-        modal.classList.add('hidden-modal');
-    }
+// Apeleaza setLanguage imediat la incarcarea paginii pentru a traduce inclusiv butoanele Meniu
+document.addEventListener('DOMContentLoaded', () => {
+    setLanguage('ro');
 });
